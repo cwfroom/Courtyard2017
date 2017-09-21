ffbfcf2ca6ebd5ddc7e90c70c23c9d3bd114e883
Wenfei Cao <admin@cwf123.com>
2017-09-20 17:17:20 -0700

    climbing WIP

---
+++ Courtyard/Source/Courtyard/CourtyardGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CourtyardGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURTYARD_API ACourtyardGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
